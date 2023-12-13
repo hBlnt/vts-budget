b539e0ab1d149e32a257cf0cd00bb7a594e3dddc
hBlnt <balinth3@gmail.com>
2023-12-13 20:37:01 +0100

    commit1phpstorm 2

--- index.php
+++ index.php
@@ -7,7 +7,28 @@
 <body>
 <?php
 echo "<p>Hello world!</p>";
-echo "<p>line2</p>"
+echo "<p>line2</p>";
+
+
+
+
+
+echo "Some more random text bnlalblalbalballalb";
+echo "BLALLABLBLABALL";
+echo "BLALLABLBLABALL";
+echo "BLALLABLBLABALL";
+echo "BLALBALBLAJJJKJKJKKJKKJLLLLLLLLLLLLJJJJKKKKJJJJHHKJKJKJJJJ";
+echo "yypyypyypciwciwciw";
+echo "yypyypyypciwciwciw";
+echo "yypyypyypciwciwciw";
+echo "ci\"";
+echo "GIT commands:";
+echo "COMMIT: CTRL+SHIFT+G -> CTRL+SHIFT+C";
+echo "PUSH: CTRL+SHIFT+G -> CTRL+SHIFT+UP";
+echo "PULL: CTRL+SHIFT+G -> CTRL+SHIFT+DOWN";
+echo "ANNOTATE: CTRL+SHIFT+G -> CTRL+SHIFT+A";
+echo "FETCH: CTRL+SHIFT+G -> CTRL+SHIFT+F";
+
 ?>
 </body>
 </html>

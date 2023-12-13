cad427d7b3396a73721360d809b371d9d4d9a11f
hBlnt <balinth3@gmail.com>
2023-12-13 20:53:14 +0100

    some generic text

--- index.php
+++ index.php
@@ -5,30 +5,14 @@
     <title>Index</title>
 </head>
 <body>
+<p>Index</p>
+<p>Some generic text like lorem ipsum here</p>
+<p>Some generic text like lorem ipsum here</p>
+<p>Some generic text like lorem ipsum here</p>
+<p>Some generic text like lorem ipsum here</p>
+<p>Some generic text like lorem ipsum here</p>
+
 <?php
-echo "<p>Hello world!</p>";
-echo "<p>line2</p>";
-
-
-
-
-
-echo "Some more random text bnlalblalbalballalb";
-echo "BLALLABLBLABALL";
-echo "BLALLABLBLABALL";
-echo "BLALLABLBLABALL";
-echo "BLALBALBLAJJJKJKJKKJKKJLLLLLLLLLLLLJJJJKKKKJJJJHHKJKJKJJJJ";
-echo "yypyypyypciwciwciw";
-echo "yypyypyypciwciwciw";
-echo "yypyypyypciwciwciw";
-echo "ci\"";
-echo "GIT commands:";
-echo "COMMIT: CTRL+SHIFT+G -> CTRL+SHIFT+C";
-echo "PUSH: CTRL+SHIFT+G -> CTRL+SHIFT+UP";
-echo "PULL: CTRL+SHIFT+G -> CTRL+SHIFT+DOWN";
-echo "ANNOTATE: CTRL+SHIFT+G -> CTRL+SHIFT+A";
-echo "FETCH: CTRL+SHIFT+G -> CTRL+SHIFT+F";
-//
 ?>
 </body>
 </html>
